e762ae8d58480204e97c2e466a62af1256957102
HicaroD <hdanrlley1@gmail.com>
2022-10-04 20:14:25 -0300

    widget: adding widget for adding a new user

---
+++ lib/widgets/add_user_form.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AddUserForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Adicionar usuário"),
+      ),
+      body: Column(children: []),
+    );
+  }
+}
